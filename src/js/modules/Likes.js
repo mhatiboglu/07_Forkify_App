6db3666e0e762386b82ae61050b473cf5571ae9c
mhatiboglu <mh1741mh@gmail.com>
2020-02-20 11:00:05 +0200

    Persist data in localstorage

--- src/js/modules/Likes.js
+++ src/js/modules/Likes.js
@@ -6,12 +6,17 @@ export default class Likes {
   addLike(id, title, author, img) {
     const like = { id, title, author, img };
     this.likes.push(like);
+    // Persist data in localstorage
+    this.persistData();
     return like;
   }
 
   deleteLike(id) {
     const index = this.likes.findIndex(el => el.id === id);
     this.likes.splice(index, 1);
+
+    // Persist data in localstorage
+    this.persistData();
   }
 
   isLiked(id) {
@@ -21,4 +26,8 @@ export default class Likes {
   getNumLikes() {
     return this.likes.length;
   }
+  persistData() {
+    localStorage.setItem("likes", JSON.stringify(this.likes));
+  }
+
 }
